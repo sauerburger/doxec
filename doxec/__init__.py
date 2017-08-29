109382c42c270e02a1753d5fae6e18591d02da35
Frank Sauerburger <frank@sauerburger.com>
2017-08-29 16:37:07 +0200

    Update version to 0.3.1

--- doxec/__init__.py
+++ doxec/__init__.py
@@ -4,7 +4,7 @@ import re
 import os
 import subprocess
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 class TestException(Exception):
     """
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="doxec",
-    version= "0.3.0",
+    version= "0.3.1",
     author="Frank Sauerburger",
     author_email= "frank@sauerburger.com",
     description=("Run documentation and test whether the examples work."),
